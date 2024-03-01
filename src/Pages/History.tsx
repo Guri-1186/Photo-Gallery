9a0a252edd31f9c8e5b21c5263be4a903697f75c
Guranda Jikia <jikiaguranda@gmail.com>
2024-03-01 12:26:00 +0400

    feat: Create routes for Main and History pages

---
+++ src/Pages/History.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const History: React.FC = () => {
+  return (
+    <div>
+      <h1>History Page</h1>
+      {/* Your history page content goes here */}
+    </div>
+  );
+};
+
+export default History;
---
+++ src/Pages/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Main: React.FC = () => {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      {/* Your main page content goes here */}
+    </div>
+  );
+};
+
+export default Main;
